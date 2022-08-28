c2c79b51439a73c80e0f9ce682882638ff27ada5
Dipankar Sarkar <deepankar.s@digitalindia.gov.in>
2022-08-28 10:17:23 +0530

    ui add started

---
+++ ui/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="Dipankar Sarkar, Ram Chaurasia">
+      <title>CoderSprint helps to showcase your skills portfolio to world.</title>
+      <!-- Bootstrap core CSS -->
+      <link href="../css/main.min.css" rel="stylesheet">
+      <style>
+         .bd-placeholder-img {
+         font-size: 1.125rem;
+         text-anchor: middle;
+         -webkit-user-select: none;
+         -moz-user-select: none;
+         user-select: none;
+         }
+         @media (min-width: 768px) {
+         .bd-placeholder-img-lg {
+         font-size: 3.5rem;
+         }
+         }
+      </style>
+   </head>
+   <body>
+      <header>
+        <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">CoderSprint</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 me-3 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="qa_home.php">Questions and Answers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="quiz_home.php">Quiz</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="app_home.php">Applications</a>
+                        </li>                                                
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Conatct</a>
+                        </li>
+                    </ul>
+                    <button class="btn btn-primary text-white fw-bold" type="button">Join Free</button>
+                    <button class="btn btn-primary text-white fw-bold" type="button">Login</button>
+                </div>
+            </div>
+        </nav>
+    </header>      
+      <main>
+         <section class="py-5 text-center container">
+            <div class="row py-lg-5">
+               <div class="col-lg-6 col-md-8 mx-auto">
+                  <h1 class="fw-light">Skilled Profiles</h1>
+                  <p class="lead text-muted">Browse through profiles to explore user skills, practice each skill with question and answers, verify skills by completing quiz and showcase your applications portfolio to world.</p>
+               </div>
+            </div>
+         </section>
+         <div class="album py-5 bg-light">
+            <div class="container">
+               <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                  <div class="col">
+                     <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                           <title>Placeholder</title>
+                           <rect width="100%" height="100%" fill="#55595c"/>
+                           <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                           <p class="card-text">Dipankar Sarkar - Full Stack developer</p>
+                           <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                                 <button type="button" class="btn btn-sm btn-outline-secondary"><a href="profile.php">View Profile</a></button>
+                              </div>
+                              <small class="text-muted">9 likes</small>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                           <title>Placeholder</title>
+                           <rect width="100%" height="100%" fill="#55595c"/>
+                           <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                           <p class="card-text">Ram Chaurasia - UI Designer</p>
+                           <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                              <button type="button" class="btn btn-sm btn-outline-secondary"><a href="profile.php">View Profile</a></button>
+                              </div>
+                              <small class="text-muted">9 likes</small>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                           <title>Placeholder</title>
+                           <rect width="100%" height="100%" fill="#55595c"/>
+                           <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                        <p class="card-text">Ram Chaurasia - UI Designer</p>
+                           <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                              <button type="button" class="btn btn-sm btn-outline-secondary"><a href="profile.php">View Profile</a></button>
+                              </div>
+                              <small class="text-muted">9 likes</small>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                           <title>Placeholder</title>
+                           <rect width="100%" height="100%" fill="#55595c"/>
+                           <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                        <p class="card-text">Ram Chaurasia - UI Designer</p>
+                           <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                              <button type="button" class="btn btn-sm btn-outline-secondary"><a href="profile.php">View Profile</a></button>
+                              </div>
+                              <small class="text-muted">9 likes</small>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                           <title>Placeholder</title>
+                           <rect width="100%" height="100%" fill="#55595c"/>
+                           <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                        <p class="card-text">Ram Chaurasia - UI Designer</p>
+                           <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                              <button type="button" class="btn btn-sm btn-outline-secondary"><a href="profile.php">View Profile</a></button>
+                              </div>
+                              <small class="text-muted">9 likes</small>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                           <title>Placeholder</title>
+                           <rect width="100%" height="100%" fill="#55595c"/>
+                           <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                        <p class="card-text">Dipankar Sarkar - Full Stack developer</p>
+                           <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                              <button type="button" class="btn btn-sm btn-outline-secondary"><a href="profile.php">View Profile</a></button>
+                              </div>
+                              <small class="text-muted">9 likes</small>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                           <title>Placeholder</title>
+                           <rect width="100%" height="100%" fill="#55595c"/>
+                           <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                        <p class="card-text">Dipankar Sarkar - Full Stack developer</p>
+                           <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                              <button type="button" class="btn btn-sm btn-outline-secondary"><a href="profile.php">View Profile</a></button>
+                              </div>
+                              <small class="text-muted">9 likes</small>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                           <title>Placeholder</title>
+                           <rect width="100%" height="100%" fill="#55595c"/>
+                           <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                        <p class="card-text">Dipankar Sarkar - Full Stack developer</p>
+                           <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                              <button type="button" class="btn btn-sm btn-outline-secondary"><a href="profile.php">View Profile</a></button>
+                              </div>
+                              <small class="text-muted">9 likes</small>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                           <title>Placeholder</title>
+                           <rect width="100%" height="100%" fill="#55595c"/>
+                           <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                        <p class="card-text">Dipankar Sarkar - Full Stack developer</p>
+                           <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                              <button type="button" class="btn btn-sm btn-outline-secondary"><a href="profile.php">View Profile</a></button>
+                              </div>
+                              <small class="text-muted">9 likes</small>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </main>
+      <footer class="text-muted py-5">
+         <div class="container">
+            <p class="float-end mb-1">
+               <a href="#">Back to top</a>
+            </p>
+            <p class="mb-1">&copy; CoderSprint. All Rights Reserved.</p>
+         </div>
+      </footer>
+      <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+   </body>
+</html>
